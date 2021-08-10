5e2e6d9f19326664c6b8c71c348c5f93fcd4a9fe
flashton2003 <flashton@gmail.com>
2021-08-10 20:36:25 +0200

    DGE working in streamlined

--- STRATAA_analysis_species_streamlined.R
+++ STRATAA_analysis_species_streamlined.R
@@ -41,7 +41,7 @@ source("/Users/flashton/Dropbox/GordonGroup/STRATAA_Microbiome_from_Leo/Leonardo
 source("/Users/flashton/Dropbox/GordonGroup/STRATAA_Microbiome_from_Leo/Leonardos_analysis/bin/diff_expr.r")
 
 
-meta <- read.csv("/Users/flashton/Dropbox/GordonGroup/STRATAA_Microbiome_from_Leo/Leonardos_analysis/0_metadata/full_meta.txt", header=T, sep = "\t")
+meta <- read.csv("/Users/flashton/Dropbox/GordonGroup/STRATAA_Microbiome_from_Leo/Leonardos_analysis/0_metadata/full_meta.tsv", header=T, sep = "\t")
 # i dont think this line does anything, probably just there for historic reasons
 names(meta)[names(meta) == "sample_ID"] <- "isolate"
 names(meta)[names(meta) == "ID"] <- "isolate"
@@ -77,4 +77,46 @@ run_calc_beta <- function(input_braken_folder, out_folder, level, country){
 run_calc_beta("/Users/flashton/Dropbox/GordonGroup/STRATAA_Microbiome_from_Leo/Leonardos_analysis/1_taxonomic_profiling/bracken_output_blantyre/species/", "/Users/flashton/Dropbox/GordonGroup/STRATAA_Microbiome_from_Leo/Leonardos_analysis/phil_blantyre/", "species", "Malawi")
 
 run_calc_beta("/Users/flashton/Dropbox/GordonGroup/STRATAA_Microbiome_from_Leo/Leonardos_analysis/1_taxonomic_profiling/bracken_output_kathmandu/species/", "/Users/flashton/Dropbox/GordonGroup/STRATAA_Microbiome_from_Leo/Leonardos_analysis/phil_kathmandu/", "species", "Nepal")
+
+run_calc_beta("/Users/flashton/Dropbox/GordonGroup/STRATAA_Microbiome_from_Leo/Leonardos_analysis/1_taxonomic_profiling/bracken_output_dhaka/species/", "/Users/flashton/Dropbox/GordonGroup/STRATAA_Microbiome_from_Leo/Leonardos_analysis/phil_dhaka/", "species", "Bangladesh")
+
+
+
+
+run_dge <- function(full_meta, root_dir, country){
+  # filter full_meta
+  full_meta <- full_meta %>% filter(Country == country)
+  sampledata <- sample_data(full_meta)
+  #OTU
+  print(paste(root_dir, "/1_species/summarised_filtered_species_otu.txt", sep = ''))
+  otu <- read.csv(paste(root_dir, "/1_species/summarised_filtered_species_otu.txt", sep = ''), header=T, row.names=1, sep = "\t")
+  colnames(otu) = gsub(pattern = "X", replacement = "", x = colnames(otu)) 
+  OTU = otu_table(otu, taxa_are_rows = TRUE)
+  
+  #put them in a phyloseq object
+  pseq <- phyloseq(OTU, sampledata)
+  full_meta <- meta(pseq)
+  otu <- abundances(pseq)
+  
+  pseq_control_vs_acute <- subset_samples(pseq, Group =="Control_HealthySerosurvey" | Group =="Acute_Typhi")
+  subset_meta <- meta(pseq_control_vs_acute)
+  subset_otu <- t(abundances(pseq_control_vs_acute))
+  #EdgeR diff expression - HERE AGE SHOULD BE ADDED 
+  result <- glm.edgeR(x=subset_meta$Group, Y=subset_otu, covariates = subset_meta[ , c('Country', 'Sex')])
+  topTags(result, n=10)
+  out_folder <- paste(root_dir,'/5_glm/', sep = '')
+  if (!dir.exists(out_folder)){ dir.create(out_folder) }
+  write.table(topTags(result, n=Inf)$table, file=paste(root_dir,'/5_glm/results_all.edgeR.tsv', sep = ''),sep='\t',quote=FALSE, col.names=NA)
+  
+}
+
+
+full_meta <- read.csv("/Users/flashton/Dropbox/GordonGroup/STRATAA_Microbiome_from_Leo/Leonardos_analysis/0_metadata/full_meta.tsv", header=T, row.names=1, sep = "\t")
+
+
+
+run_dge(full_meta, '/Users/flashton/Dropbox/GordonGroup/STRATAA_Microbiome_from_Leo/Leonardos_analysis/phil_blantyre', 'Malawi')
+
+
+
 
