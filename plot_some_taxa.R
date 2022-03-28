4f5b9217a6bc44ce6a9669d031cfd68cc31c97e5
flashton2003 <flashton@gmail.com>
2022-03-28 20:43:08 +0200

    changing to function design

--- plot_some_taxa.R
+++ plot_some_taxa.R
@@ -12,35 +12,40 @@ library(patchwork)
 '''
 
 data_table.normalised.clr <- read.csv("/Users/flashton/Dropbox/GordonGroup/STRATAA_Microbiome/from_leo/Leonardos_analysis/phil_running_3/1_normalized_data/clr_normalised_data.txt", header=T, sep = "\t")
-
 #data_table.normalised.clr <- read_delim("/Users/flashton/Dropbox/GordonGroup/STRATAA_Microbiome/from_leo/Leonardos_analysis/phil_running_3/1_normalized_data/clr_normalised_data.txt", delim = "\t", escape_double = FALSE, trim_ws = TRUE)
-
 colnames(data_table.normalised.clr) = gsub(pattern = "X", replacement = "", x = colnames(data_table.normalised.clr)) 
 data_table.normalised.clr <- as.data.frame(t(data_table.normalised.clr))
 colnames(data_table.normalised.clr) = gsub(pattern = "X", replacement = "", x = colnames(data_table.normalised.clr)) 
 data_table.normalised.clr$taxon <- rownames(data_table.normalised.clr)
 rownames(data_table.normalised.clr) <- NULL
 
+
+
+data_table.raw_counts <- read.csv("/Users/flashton/Dropbox/GordonGroup/STRATAA_Microbiome/from_leo/Leonardos_analysis/phil_running_3/1_species/summarised_filtered_species_otu.tsv", header=T, sep = "\t")
+names(data_table.raw_counts)[names(data_table.raw_counts) == 'X'] <- 'taxon'
+#rownames(data_table.raw_counts) <- data_table.raw_counts$X
+#data_table.raw_counts <- subset(data_table.raw_counts, select = -c(X))
+colnames(data_table.raw_counts) = gsub(pattern = "X", replacement = "", x = colnames(data_table.raw_counts)) 
+
+data_table <- data_table.raw_counts
+y_lab_text <- 'raw counts'
+data_table <- data_table.normalised.clr
+y_lab_text <- 'CLM transformed reads'
+
+
 meta <- read.csv("/Users/flashton/Dropbox/GordonGroup/STRATAA_Microbiome/from_leo/Leonardos_analysis/0_metadata/full_meta.tsv", header=T, sep = "\t")
+data_table <- pivot_longer(data_table, !taxon)
+data_table <- data_table %>% left_join(select(meta, c('ID', 'Group', 'Country')), by = c('name' = 'ID'))
+healthy_taxa <- c('Prevotella.copri', 'Dialister.sp000434475', 'Prevotella copri', 'Dialister sp000434475')
+typhi_taxa <- c( 'Absiella.innocuum', 'Pauljensenia.odontolyticus', 'KLE1796.sp001580115', 'Massiliomicrobiota.timonensis', 'Absiella innocuum', 'Pauljensenia odontolyticus', 'KLE1796 sp001580115', 'Massiliomicrobiota timonensis')
+data_table <- filter(data_table, Country != 'Nepal')
+data_table <- filter(data_table, Group != 'Carrier')
+healthy_all <- filter(data_table, taxon %in% healthy_taxa)
+typhi_all <- filter(data_table, taxon %in% typhi_taxa)
 
+p_healthy_all <- ggplot(healthy_all, aes(x = taxon, y = value, fill = factor(Group))) + geom_boxplot() + coord_flip() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=0.5)) + coord_flip() + ylab(y_lab_text) + xlab('Species')
 
-data_table.normalised.clr <- pivot_longer(data_table.normalised.clr, !taxon)
-
-
-data_table.normalised.clr <- data_table.normalised.clr %>% left_join(select(meta, c('ID', 'Group', 'Country')), by = c('name' = 'ID'))
-
-healthy_taxa <- c('Prevotella.copri', 'Dialister.sp000434475')
-typhi_taxa <- c( 'Absiella.innocuum', 'Pauljensenia.odontolyticus', 'KLE1796.sp001580115', 'Massiliomicrobiota.timonensis')
-
-
-data_table.normalised.clr <- filter(data_table.normalised.clr, Country != 'Nepal')
-
-data_table.normalised.clr <- filter(data_table.normalised.clr, Group != 'Carrier')
-healthy_all <- filter(data_table.normalised.clr, taxon %in% healthy_taxa)
-typhi_all <- filter(data_table.normalised.clr, taxon %in% typhi_taxa)
-
-p_healthy_all <- ggplot(healthy_all, aes(x = taxon, y = value, fill = factor(Group))) + geom_boxplot() + coord_flip() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=0.5)) + coord_flip() + ylab('CLM transformed reads') + xlab('Species')
-p_typhi_all <- ggplot(typhi_all, aes(x = taxon, y = value, fill = Group)) + geom_boxplot() + coord_flip() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=0.5)) + coord_flip() + ylab('CLM transformed reads') + xlab('Species')
+p_typhi_all <- ggplot(typhi_all, aes(x = taxon, y = value, fill = Group)) + geom_boxplot() + coord_flip() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=0.5)) + coord_flip() + ylab(y_lab_text) + xlab('Species')
 
 p_healthy_all / p_typhi_all + plot_layout(heights = c(2, 4))
 
