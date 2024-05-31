7bbc8fedac3977438b6ab7edafe7d2af9ea95eb3
flashton2003 <flashton@gmail.com>
2024-05-31 15:14:57 +0200

    restructured

--- config.R
+++ config.R
@@ -5,3 +5,5 @@ maaslin_functional_output_root_folder <- '/Users/flashton/Dropbox/GordonGroup/ST
 patch_metadata_handle <- "~/Dropbox/GordonGroup/STRATAA_Microbiome/patch_data/metaphlan/results/2023.05.05/samples_n383_allMetadata_SubsettedNewVariables_040318_KH.csv"
 patch_metaphlan_handle <- "~/Dropbox/GordonGroup/STRATAA_Microbiome/patch_data/metaphlan/results/2023.05.05/2023.05.05.patch.metaphlan.csv"
 patch_maaslin_taxonomic_output_root_folder <- "/Users/flashton/Dropbox/GordonGroup/STRATAA_Microbiome/patch_data/metaphlan/results/2024.01.10"
+three_qual_colours <- brewer.pal(3, "Dark2")
+participant_group_colours <- c("Acute typhoid" = three_qual_colours[[2]], "Household contact" = three_qual_colours[[1]], "High Vi-titre" = three_qual_colours[[3]])
